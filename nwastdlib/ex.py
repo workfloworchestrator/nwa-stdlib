1b452f6e2738ecdd81eb99a6aab8f319f550f06f
Bart Bakker <bart@thesoftwarecraft.com>
2017-05-04 13:00:25 +0200

    Add standardized exception format functions

---
+++ nwastdlib/ex.py
@@ -0,0 +1,32 @@
+"""Module containing functions to deal with `Exception`s"""
+
+import random
+import string
+import traceback
+
+
+def format_ex(ex, stacklimit=5):
+    '''
+    Format an exception with a pseudo-random key and the shown exception.
+
+    Returns a tuple of the exception string and key.
+    '''
+    key = "".join(random.choices(string.ascii_letters + string.digits, k=6))
+    s = show_ex(ex, stacklimit)
+    return (key, "[%s] %s" % (key, s))
+
+
+def show_ex(ex, stacklimit=5):
+    '''
+    Show an exception, including its class name, message and (limited) stack
+    trace.
+
+    >>> try:
+    ...     raise Exception("Something went wrong")
+    ... except Exception as e:
+    ...     print(show_ex(e))
+    Exception: Something went wrong
+    ...
+    '''
+    tbfmt = "".join(traceback.format_tb(ex.__traceback__, stacklimit))
+    return "%s: %s\n%s" % (type(ex).__name__, ex, tbfmt)
