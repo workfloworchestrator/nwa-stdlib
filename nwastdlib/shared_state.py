2dcdb0dd0c1fbfe4711f6675ed302b51d100e77c
Bart Bakker <bart@thesoftwarecraft.com>
2017-06-07 16:18:43 +0200

    Add imperative state sharing metaclass

---
+++ nwastdlib/shared_state.py
@@ -0,0 +1,36 @@
+class SharedState(type):
+    '''
+    Type to imperatively share object state between modules.
+
+    Use `SharedState` as the metaclass of your own imperative state class. Note
+    that constructor of a state object is invoked only once.
+
+    >>> class MyState(metaclass=SharedState):
+    ...     pass
+
+    Every "instance" then yields exactly the same object.
+
+    >>> id(MyState()) == id(MyState())
+    True
+
+    >>> class MyNewState(MyState):
+    ...     pass
+
+    Derived state objects are unique too.
+
+    >>> id(MyNewState()) == id(MyNewState())
+    True
+
+    However, derived state object are /different/ from their parent.
+
+    >>> id(MyState()) == id(MyNewState())
+    False
+    '''
+
+    __instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        instance = cls.__instances.get(cls)
+        if instance is None:
+            instance = cls.__instances[cls] = super(SharedState, cls).__call__(*args, **kwargs)
+        return instance
