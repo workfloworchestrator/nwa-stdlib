aea5aff315d6a3e43bafbac1189cada71583a2f5
Bart Bakker <bart@thesoftwarecraft.com>
2017-06-12 15:09:54 +0200

    Add `find` function for lists
    Also split generic functions `f.py` and list functions `list.py` to allow using
    `Maybe` from list functions.

--- nwastdlib/f.py
+++ nwastdlib/f.py
@@ -33,7 +33,8 @@ def compose(f: Callable[[β], γ], g: Callable[[α], β]) -> Callable[[α], γ]:
     '''
     Get the composition f of g
 
-    >>> compose(fst, list)({2,3,4})
+    >>> head = lambda xs: xs[0]
+    >>> compose(head, list)({2,3,4})
     2
     '''
     return lambda x: f(g(x))
@@ -84,54 +85,3 @@ def unkwargs(f):
     (1, 2, 3)
     '''
     return lambda x: f(**x)
-
-
-def fst(xs):
-    '''
-    Get the first element from an indexed iterable data structure (e.g. tuple,
-    list).
-
-    Note: this depends on bounds checking of the data structure. Most iterable
-    data structures in Python raise an IndexError when the index is out of
-    bounds.
-
-    >>> fst([1,2,3])
-    1
-
-    >>> fst(())
-    Traceback (most recent call last):
-        ...
-    IndexError: tuple index out of range
-    '''
-    return xs[0]
-
-
-def snd(xs):
-    '''
-    Get the second element from an indexed iterable data structure (e.g. tuple,
-    list).
-
-    Note: this depends on bounds checking of the iterable data structure. Most
-    iterable data structures in Python raise an IndexError when the index is
-    out of bounds.
-
-    >>> snd([1,2,3])
-    2
-    '''
-    return xs[1]
-
-
-def elem(x, xs):
-    '''
-    Get whether `x` is in `xs`.
-
-    >>> elem(1, range(1,100))
-    True
-
-    >>> elem(100, range(1,100))
-    False
-    '''
-    try:
-        return xs.index(x) >= 0
-    except ValueError:
-        return False
---
+++ nwastdlib/list.py
@@ -0,0 +1,76 @@
+'''
+Module containing utility functions for lists.
+'''
+from typing import Callable, List, TypeVar
+
+from .maybe import Maybe
+
+α = TypeVar('a')
+
+
+def fst(xs: List[α]) -> α:
+    '''
+    Get the first element from an indexed iterable data structure (e.g. tuple,
+    list).
+
+    Note: this depends on bounds checking of the data structure. Most iterable
+    data structures in Python raise an IndexError when the index is out of
+    bounds.
+
+    >>> fst([1,2,3])
+    1
+
+    >>> fst(())
+    Traceback (most recent call last):
+        ...
+    IndexError: tuple index out of range
+    '''
+    return xs[0]
+
+
+def snd(xs: List[α]) -> α:
+    '''
+    Get the second element from an indexed iterable data structure (e.g. tuple,
+    list).
+
+    Note: this depends on bounds checking of the iterable data structure. Most
+    iterable data structures in Python raise an IndexError when the index is
+    out of bounds.
+
+    >>> snd([1,2,3])
+    2
+    '''
+    return xs[1]
+
+
+def elem(x: α, xs: List[α]) -> bool:
+    '''
+    Get whether `x` is in `xs`.
+
+    >>> elem(1, range(1,100))
+    True
+
+    >>> elem(100, range(1,100))
+    False
+    '''
+    try:
+        return xs.index(x) >= 0
+    except ValueError:
+        return False
+
+
+def find(p: Callable[[α], bool], xs: List[α]) -> Maybe[α]:
+    '''
+    Find the first element in `xs` that matches the predicate `p`.
+
+    >>> find(lambda x: x > 1, [1,2,3])
+    Some 2
+
+    >>> find(lambda x: x < 1, [1,2,3])
+    Nothing
+    '''
+    g = (x for x in xs if p(x))
+    try:
+        return Maybe.Some(next(g))
+    except StopIteration:
+        return Maybe.Nothing()
