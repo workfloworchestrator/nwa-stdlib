4e4bfc743df7f8b327ddbf35abc3b1bfad2c479c
Hans Trompert <hans.trompert@surfnet.nl>
2017-07-26 13:47:14 +0200

    remove debug request headers

--- nwastdlib/cache.py
+++ nwastdlib/cache.py
@@ -9,7 +9,6 @@ from collections import namedtuple
 import redis
 import connexion
 import json
-import sys
 
 Error = namedtuple("Error", ["status", "key", "message"])
 
@@ -29,7 +28,6 @@ def handle_query(pool):
     key = connexion.request.full_path
 
     def resp_parser(pool):
-        print(connexion.headers, file=sys.stderr)
         if connexion.headers.get('nwa-stdlib-no-cache'):
             return Either.Left(None)
         return Maybe.of(pool.get(key))\
