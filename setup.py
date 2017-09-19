8d9ddb10b8f35b2736ca2318ebf420c04a9d4bcc
oharsta <oharsta@zilverline.com>
2017-09-19 15:25:28 +0200

    flake8 - missed top level check

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup_params = dict(
     version='0.2',
     packages=['nwastdlib'],
     install_requires=['pytz==2016.6', 'pyyaml==3.12', 'redis==2.10.5', 'hiredis==0.2.0', 'Flask==0.12.2',
-                      'Flask-Testing==0.6.2','requests-mock==1.3.0'],
+                      'Flask-Testing==0.6.2', 'requests-mock==1.3.0'],
     extras_require={
         "mq": ['stomp.py==4.1'],
         "api": ['connexion==1.1'],
