77e4e002f2c5e1f03abcedf5aef0c514fcbe93ee
Bart Bakker <bart@thesoftwarecraft.com>
2017-10-03 11:52:37 +0200

    OAuth filter uses 'requests'
    Also move test requirements to 'test_require' config.

--- setup.py
+++ setup.py
@@ -6,15 +6,14 @@ setup_params = dict(
     name='nwa-stdlib',
     version='0.2',
     packages=setuptools.find_packages(exclude=["*.test", "*.test.*"]),
-    install_requires=['pytz==2017.2', 'pyyaml==3.12', 'redis==2.10.6', 'hiredis==0.2.0', 'Flask==0.12.2',
-                      'Flask-Testing==0.6.2', 'requests-mock==1.3.0'],
+    install_requires=['pytz==2017.2', 'pyyaml==3.12', 'redis==2.10.6', 'hiredis==0.2.0', 'Flask==0.12.2', 'requests==2.18.4'],
     extras_require={
         "mq": ['stomp.py==4.1'],
         "api": ['connexion==1.1'],
         "redis": ['redis==2.10.5', 'hiredis==0.2.0']
     },
     setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
+    tests_require=['pytest', 'Flask-Testing==0.6.2', 'requests-mock==1.3.0'],
     description='Network Automation Standard Library',
     long_description='Network Automation Standard Library',
     author='SURFnet NOC',
