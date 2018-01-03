f6c59a1df879b1189fbe5d2f18e250402c203473
oharsta <oharsta@zilverline.com>
2018-01-03 13:54:21 +0100

    Retain original error status

--- nwastdlib/api_client.py
+++ nwastdlib/api_client.py
@@ -58,7 +58,7 @@ def run_api_call(name, get_client):
         if e.status == 404:
             return Either.Left(Error(404, key, "Not found"))
         if elem(e.status, range(400, 500)):
-            return Either.Left(Error(500, key, f"Error communicating to {name}. Response status code was {e.status} with payload: {e.body}"))
+            return Either.Left(Error(e.status, key, f"Error communicating to {name}. Response status code was {e.status} with payload: {e.body}"))
         if elem(e.status, range(500, 600)):
             return Either.Left(Error(e.status, key, f"Received server error {e.status} from {name} with payload: {e.body}"))
         return Either.Left(Error(e.status, key, "Error while accessing %s" % name))
