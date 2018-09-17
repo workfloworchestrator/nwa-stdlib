c5f2b053d78937fa735a863cf7b69d9257d5418a
Guido Kollerie <guido@kollerie.com>
2018-09-17 14:39:04 +0200

    Make `pip install -e .[test]` work.

--- setup.py
+++ setup.py
@@ -2,17 +2,20 @@
 
 import setuptools
 
+test_dependencies = ['pytest', 'Flask-Testing==0.7.1', 'requests-mock==1.4.0', 'fakeredis==0.10.3']
 setup_params = dict(
     name='nwastdlib',
     version='0.9.19',
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires=['pytz==2018.5', 'ruamel.yaml~=0.15.66', 'redis==2.10.6', 'hiredis==0.2.0', 'Flask>=1.0.2', 'requests==2.18.4'],
+    install_requires=['pytz==2018.5', 'ruamel.yaml~=0.15.66', 'redis==2.10.6', 'hiredis==0.2.0', 'Flask>=1.0.2',
+                      'requests==2.18.4'],
     extras_require={
-        "redis": ['redis==2.10.6', 'hiredis==0.2.0']
+        "redis": ['redis==2.10.6', 'hiredis==0.2.0'],
+        "test": test_dependencies
     },
     setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'Flask-Testing==0.7.1', 'requests-mock==1.4.0', 'fakeredis==0.10.3'],
+    tests_require=test_dependencies,
     description='Network Automation Standard Library',
     long_description='Network Automation Standard Library',
     author='SURFNet Automation',
