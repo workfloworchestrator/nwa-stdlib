66ec4a070a2fff0ebec57589d4ee0640305d455c
René Dohmen <acidjunk@gmail.com>
2023-01-27 23:56:40 +0100

    Disabled benchmark WIP

--- benchmarks/benchmark_async_cache_with_pickle.py
+++ benchmarks/benchmark_async_cache_with_pickle.py
@@ -1,25 +1,25 @@
-import asyncio
-import random
-import timeit
-
-from redis.asyncio import Redis as AIORedis
-from shared import SIZE
-
-from nwastdlib.asyncio_cache import cached_result
-
-cache: AIORedis = AIORedis(host="127.0.0.1", port=6379)
-
-
-@cached_result(cache, "test-suite", "SECRETNAME", "keyname", expiry_seconds=2000)
-async def big_dict():
-    result = {}
-    for i in range(0, SIZE):
-        result[i] = random.randint(0, SIZE * 100)
-    return result
-
-
-# warm the cache:
-result = await big_dict()
-
-timeit.repeat(await big_dict(), repeat=100)
-asyncio.run()
+# import asyncio
+# import random
+# import timeit
+#
+# from redis.asyncio import Redis as AIORedis
+# from shared import SIZE
+#
+# from nwastdlib.asyncio_cache import cached_result
+#
+# cache: AIORedis = AIORedis(host="127.0.0.1", port=6379)
+#
+#
+# @cached_result(cache, "test-suite", "SECRETNAME", "keyname", expiry_seconds=2000)
+# async def big_dict():
+#     result = {}
+#     for i in range(0, SIZE):
+#         result[i] = random.randint(0, SIZE * 100)
+#     return result
+#
+#
+# # warm the cache:
+# result = await big_dict()
+#
+# timeit.repeat(await big_dict(), repeat=100)
+# asyncio.run()
