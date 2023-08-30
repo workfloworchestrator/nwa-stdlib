8736a246b342f985998cfa12ea9c39273507d631
Maurits Rijk <maurits.rijk@surf.nl>
2023-08-30 14:21:56 +0200

    black

--- nwastdlib/settings.py
+++ nwastdlib/settings.py
@@ -11,6 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from pydantic import VERSION
+
 if VERSION > "2.0":
     from pydantic_settings import BaseSettings
 else:
